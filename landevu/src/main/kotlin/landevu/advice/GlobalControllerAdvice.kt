14a9b40c01b4d06399e7dfb2a6bac2a54c2405c0
j <penguinjinbei@gmail.com>
2023-06-09 09:12:43 +0900

    feat: set error response status in advice

--- landevu/src/main/kotlin/landevu/advice/GlobalControllerAdvice.kt
+++ landevu/src/main/kotlin/landevu/advice/GlobalControllerAdvice.kt
@@ -1,12 +1,15 @@
 package landevu.advice
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 @Component
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 class GlobalControllerAdvice {
     @ExceptionHandler(Exception::class)
     fun log(e: Exception, model: Model) {
