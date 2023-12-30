d7529c0caf77101c67f0fc7aafc11a2c73c39bc9
j <penguinjinbei@gmail.com>
2023-12-30 19:07:14 +0900

    feat: train_line_id, station_idの桁数を減らす

--- docker/db/initdb/1_create_table.sql
+++ docker/db/initdb/1_create_table.sql
@@ -12,13 +12,13 @@ create table spots (
     foreign key (area_id) references areas(area_id)
 );
 create table train_lines (
-    train_line_id char(10) primary key,
+    train_line_id char(5) primary key,
     train_line_name varchar(100) not null
 );
 create table stations (
-    station_id char(10) primary key,
+    station_id char(8) primary key,
     station_name varchar(100) not null,
-    train_line_id char(10),
+    train_line_id char(5),
     area_id char(10),
     foreign key (train_line_id) references train_lines(train_line_id),
     foreign key (area_id) references areas(area_id)
