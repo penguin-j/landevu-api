7cf25f93b12932e36ca28822b9ac7ed597e841ca
j <penguinjinbei@gmail.com>
2023-12-30 18:18:48 +0900

    feat: テーブル名を複数形に変更

--- docker/db/initdb/1_create_table.sql
+++ docker/db/initdb/1_create_table.sql
@@ -1,3 +1,3 @@
 -- テーブル作成
-create table area (area_id char(10) primary key, area_name varchar(10) not null);
-create table spot (spot_id char(10) primary key, spot_name varchar(100) not null, area_id char(10) not null, longitude numeric not null, latitude numeric not null);
+create table areas (area_id char(10) primary key, area_name varchar(10) not null);
+create table spots (spot_id char(10) primary key, spot_name varchar(100) not null, area_id char(10) not null, longitude numeric not null, latitude numeric not null);
