c940dacccdcf2212c49709122a7aaa95165b4d6d
j <penguinjinbei@gmail.com>
2023-12-30 18:19:49 +0900

    feat: テーブル定義に外部キー制約を追加

--- docker/db/initdb/1_create_table.sql
+++ docker/db/initdb/1_create_table.sql
@@ -1,3 +1,13 @@
 -- テーブル作成
-create table areas (area_id char(10) primary key, area_name varchar(10) not null);
-create table spots (spot_id char(10) primary key, spot_name varchar(100) not null, area_id char(10) not null, longitude numeric not null, latitude numeric not null);
+create table areas (
+    area_id char(10) primary key,
+    area_name varchar(10) not null
+);
+create table spots (
+    spot_id char(10) primary key,
+    spot_name varchar(100) not null,
+    area_id char(10) not null,
+    longitude numeric not null,
+    latitude numeric not null,
+    foreign key (area_id) references areas(area_id)
+);
