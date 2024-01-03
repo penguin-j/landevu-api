0769f1f7c38353fcaaedb5ff6f9bf4a49dc3a2e2
j <penguinjinbei@gmail.com>
2024-01-03 13:47:23 +0900

    chore: DBスキーマを変更

--- docker/db/initdb/1_create_table.sql
+++ docker/db/initdb/1_create_table.sql
@@ -1,25 +1,25 @@
 -- テーブル作成
-create table areas (
+create table landevu.areas (
     area_id char(10) primary key,
     area_name varchar(10) not null
 );
-create table spots (
+create table landevu.spots (
     spot_id char(10) primary key,
     spot_name varchar(100) not null,
     area_id char(10) not null,
     longitude numeric not null,
     latitude numeric not null,
-    foreign key (area_id) references areas(area_id)
+    foreign key (area_id) references landevu.areas(area_id)
 );
-create table train_lines (
+create table landevu.train_lines (
     train_line_id char(5) primary key,
     train_line_name varchar(100) not null
 );
-create table stations (
+create table landevu.stations (
     station_id char(8) primary key,
     station_name varchar(100) not null,
     train_line_id char(5),
     area_id char(10),
-    foreign key (train_line_id) references train_lines(train_line_id),
-    foreign key (area_id) references areas(area_id)
+    foreign key (train_line_id) references landevu.train_lines(train_line_id),
+    foreign key (area_id) references landevu.areas(area_id)
 );
